bd6abe1ef3c12dfec6d0410bc896425deecf6a21
Dana Macias <dsofiamrojasd6419@gmail.com>
2025-10-18 11:06:56 -0500

    Primer commit app Pit Stops

---
+++ app/src/main/java/com/example/pit_stops/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.pit_stops
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.layout.ContentScale
+import androidx.core.graphics.toColorInt
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            inicio()
+        }
+    }
+
+}
+
+@Composable
+fun inicio(){
+
+}
